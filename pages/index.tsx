f35c156f034b0783ee97b28e0995eedcb54fb093
Adrian Puczyłowski <49859031+adriaan-code@users.noreply.github.com>
2023-07-27 18:54:06 +0200

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import MainView from "@/components/mainView";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <MainView />
+    </div>
+  );
+}
